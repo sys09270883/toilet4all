a9c263a30ebe2d41557302b5f52e5b08c0599589
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-06-05 21:11:23 +0900

    게시판 홈 버튼 백스택 처리 완료

--- app/src/main/java/com/example/toilet4all/BoardActivity.kt
+++ app/src/main/java/com/example/toilet4all/BoardActivity.kt
@@ -24,10 +24,11 @@ class BoardActivity : AppCompatActivity() {
         boardContentList.add(Board("첫 번째 글", "첫 번째 글의 내용입니다.", "2020-06-05", "신윤섭"))
         boardContentList.add(Board("두 번째 글", "두 번째 글의 내용입니다.", "2020-06-05", "김영경"))
         boardContentList.add(Board("세 번째 글", "세 번째 글의 내용입니다.", "2020-06-05", "김아지"))
-        boardContentList.add(Board("네 번째 글", "네 번째 글의 내용입니다.", "2020-06-05", "김아지"))
+        boardContentList.add(Board("네 번째 글", "네 번째 글의 내용입니다.", "2020-06-05", "다현"))
         boardContentList.add(Board("다섯 번째 글", "다섯 번째 글의 내용입니다.", "2020-06-05", "쯔위"))
         boardContentList.add(Board("여섯 번째 글", "여섯 번째 글의 내용입니다.", "2020-06-05", "나연"))
         boardContentList.add(Board("일곱 번째 글", "일곱 번째 글의 내용입니다.", "2020-06-05", "사나"))
+        boardContentList.add(Board("여덟 번째 글", "여덟 번째 글의 내용입니다.", "2020-06-05", "모모"))
         /* end test */
 
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -46,6 +47,9 @@ class BoardActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, layoutManager.orientation))
 
+        homeButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 
     fun toast(position: Int) {
